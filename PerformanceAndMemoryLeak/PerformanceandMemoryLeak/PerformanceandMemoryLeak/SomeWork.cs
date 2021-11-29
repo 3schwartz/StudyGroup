69053620e9aa439ba42a7aa6b153e523ffb3ca7a
Søren Schwartz <sosc@netcompany.com>
2021-11-29 08:51:46 +0100

    Added hosted service

---
+++ PerformanceAndMemoryLeak/PerformanceandMemoryLeak/PerformanceandMemoryLeak/SomeWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace PerformanceandMemoryLeak
+{
+    public class SomeWork : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Task.Run(DoSomething, cancellationToken);
+
+
+            return Task.CompletedTask;
+        }
+
+        public Task DoSomething()
+        {
+            var imCool = true;
+
+            while (imCool)
+            {
+                // Do something
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            // Do nothing
+
+            return Task.CompletedTask;
+        }
+    }
+}
